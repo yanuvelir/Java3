4a23fc45120cec0d272f9ae8394a8e55da670132
yanuvelir <yanuvelir@gmail.com>
2022-02-09 12:07:07 +0200

    Initial commit

---
+++ src/main/java/MyNotFirstTest.java
@@ -0,0 +1,15 @@
+public class MyNotFirstTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Wood");
+
+        int i = 2;
+        int b = 3;
+
+        System.out.println(i);
+        System.out.println(b);
+
+
+
+    }
+}
