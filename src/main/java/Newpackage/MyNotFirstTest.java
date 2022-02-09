3f710ebfe397d5e4575315dd1a603a8ca7214016
yanuvelir <yanuvelir@gmail.com>
2022-02-09 14:16:00 +0200

    Easy Math

--- src/main/java/Newpackage/MyNotFirstTest.java
+++ src/main/java/Newpackage/MyNotFirstTest.java
@@ -5,12 +5,30 @@ public class MyNotFirstTest {
     public static void main(String[] args) {
         System.out.println("Hi Wood");
 
-        int i = 2;
-        int b = 3;
+        //int i = 2;
+        //int b = 3;
+        //System.out.println(i);
+        //System.out.println(b);
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
 
-        System.out.println(i);
-        System.out.println(b);
-        System.out.println("try to push");
+         System.out.println(1/2);
+        System.out.println(1.0 / 2);
+        System.out.println(1/ 2.0);
+        System.out.println(2.0/2);
+
+        System.out.println("2"+"2");
+        System.out.println("2"+2);
+        System.out.println(2+"2");
+
+        System.out.println(2+2*2);
+        System.out.println((2+2)*2);
+        System.out.println("2+2="+(2+2));
+        System.out.println("2+2"+(2+2));
+        System.out.println("2+2+3");
+
 
 
     }
